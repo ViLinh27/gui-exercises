b83d5bd4e8decacaecb345a570414114408a8efc
Vi-Linh Nguyen <vilinh27@gmail.com>
2023-01-08 21:32:14 -0600

    new calculator file to fiddle with

---
+++ src/Calculator.java
@@ -0,0 +1,89 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculator {
+    public static void main(String[] args){//frontend
+        //main frame
+        JFrame frame = new JFrame("Number Addition");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,400);
+
+        //panel
+        JPanel panel = new JPanel();
+        JPanel panel2 = new JPanel();
+
+        //labels and text fields
+        JLabel lb1 = new JLabel("First Number: ");
+        JTextField tf1 = new JTextField(10);
+        JLabel lb2 = new JLabel("Second Number");
+        JTextField tf2 = new JTextField(10);
+        JLabel lb3 = new JLabel("Result: ");
+        JTextField tf3 = new JTextField(10);
+        panel.add(lb1);
+        panel.add(tf1);
+        panel.add(lb2);
+        panel.add(tf2);
+        panel.add(lb3);
+        panel.add(tf3);
+        panel.setLayout(new GridLayout(3,2));
+
+        //buttons
+        JButton btn = new JButton("Add");
+        JButton btn2 =  new JButton("Clear");
+        JButton btn3 = new JButton("Exit");
+        JButton btn_sub = new JButton("Substract");
+        panel2.add(btn);
+        panel2.add(btn_sub);
+        panel2.add(btn2);
+        panel2.add(btn3);
+        panel2.setLayout(new GridLayout(2,2));
+
+        //action listener
+        btn3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ExitButtonAction();
+            }
+        });
+        btn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tf1.setText("");
+                tf2.setText("");
+                tf3.setText("");
+            }
+        });
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //accept input from tf1 and tf2, convert to float
+                float num1,num2,result;
+                num1 = Float.parseFloat(tf1.getText());
+                num2 = Float.parseFloat(tf2.getText());
+                result = num1+num2;
+                tf3.setText(String.valueOf(result));
+                //perform addtion
+                //convert sum to string to put in tf3
+            }
+        });
+        btn_sub.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                float num1,num2,result;
+                num1 = Float.parseFloat(tf1.getText());
+                num2 = Float.parseFloat(tf2.getText());
+                result = num1-num2;
+                tf3.setText(String.valueOf(result));
+            }
+        });
+
+        frame.getContentPane().add(BorderLayout.NORTH,panel);
+        frame.getContentPane().add(BorderLayout.SOUTH,panel2);
+        frame.setVisible(true);
+    }
+    private static void ExitButtonAction(){
+        System.exit(0);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,52 @@
 import javax.swing.*;
+import java.awt.*;
 public class Main {
     public static void main(String[] args) {
-
+        //the main frame
         JFrame frame = new JFrame("My first GUI in java");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,600);
-        JButton button = new JButton("Press");
+        frame.setSize(300,300);
 
-        frame.getContentPane().add(button);//adds button to content pane of frame
+        //menubar with components
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu1 = new JMenu("FILE");
+        JMenu menu2 = new JMenu("HELP");
+        menuBar.add(menu1);
+        menuBar.add(menu2);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        JMenuItem m33 = new JMenuItem("Settings");
+        JMenuItem m44 = new JMenuItem("Preferences");
+        menu1.add(m11);
+        menu1.add(m22);
+        menu2.add(m33);
+        menu2.add(m44);
+
+        //buttons
+        //JButton button = new JButton("Press 1");
+        //JButton button2 = new JButton("Press 2");
+        //frame.getContentPane().add(button);//adds button to content pane of frame
+        //frame.getContentPane().add(button2);
+        //frame.setVisible(true);
+
+        //panel at the bottom with components
+        JPanel panel = new JPanel();//panel not visible in output
+        JLabel label = new JLabel("Enter Text Here: ");
+        JTextField tf = new JTextField(10);//accepts 10 characters here
+        JButton send = new JButton("Send");
+        JButton resetbtn = new JButton("Reset");
+        panel.add(label);//components added using FLow Layout//single row layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(resetbtn);
+
+        //text area
+        JTextArea ta = new JTextArea();
+
+        //adding components to frame;
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, menuBar);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
         frame.setVisible(true);
     }
 }
